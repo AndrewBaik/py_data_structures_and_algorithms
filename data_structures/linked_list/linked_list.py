ea3f8be97cedb363f98242639de7d82a077cfe5f
Andrew Baik <andrewbaik@outlook.com>
2018-08-20 09:14:17 -0700

    resubmit linked list

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -2,7 +2,11 @@ from .node import Node
 
 
 class LinkedList(object):
+    """ Abstract clss for linked list
+    """
     def __init__(self, val_list=None):
+        """ Constructor for LinkedList
+        """
         self.head = None
         self._length = 0
 
@@ -20,6 +24,8 @@ class LinkedList(object):
         return self._length
 
     def insert(self, val):
+        """ Inserting a Node created according to its value inserted to this method
+        """
         new_node = Node(val)
         new_node._next = self.head
         self.head = new_node
@@ -27,12 +33,23 @@ class LinkedList(object):
         self._length += 1
 
     def includes(self, val) -> bool:
+        """ Validates if inputed value is included in the linked list
+        """
         self.current = self.head
         while self.current is not None:
             if self.current.val is val:
                 return True
             self.current = self.current._next
         return False
+
+    def append(self, value):
+        """ method that adds a new node at the end of the linked list
+        """
+        self.current = self.head
+        while self.current._next is not None:
+            self.current = self.current._next
+        new_node = Node(value)
+        self.current._next = new_node
 
 
 def run():
@@ -44,3 +61,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
