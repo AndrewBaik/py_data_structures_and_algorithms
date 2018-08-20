8c3097ca8be5b5d9e1454b93c1e5e3a26bab85c1
Andrew Baik <andrewbaik@outlook.com>
2018-08-20 15:12:50 -0700

    Implemented append, insert before/after

--- data_structures/linked_list/README.md
+++ data_structures/linked_list/README.md
@@ -10,3 +10,10 @@
 - Implement insert(), when method gets called with an argument of an integer, a new Node gets created and attached in the head of linked list.
 - Implemented includes(), when method gets called with an argument of an integer, it will iterate through the linked list to find the input integer equal to the Node value within the linked list and return a boolean.
 - Implement constructor that has an option to take a list of integer. Then it will create a Nodes with the values included in the list and insert it into the linked list.
+- Implement `append(value)`, which takes an argument of a value, create a new Node and append it at the end of the Linked List.
+- Implement `insertBefore(value, newValue)`, that takes two argument of an existing value and a new value. It will iterate and find a Node that holds the existing value, and create a new Node with a value of a new value and insert it before the Node with existing value.
+- Implement `insertAfter(value, new Value)`, this will do the same action as `insertBefore`, but the new Node will insert it after the Node with existing value.
+
+## Whiteboard
+![append and insert whiteboard1](../../assets/linkedlist-insert1.jpg)
+![append and insert whiteboard2](../../assets/linkedlist-insert2.jpg)
--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -1,4 +1,4 @@
-from .node import Node
+from node import Node
 
 
 class LinkedList(object):
@@ -14,7 +14,7 @@ class LinkedList(object):
             for val in val_list:
                 self.insert(val)
 
-    def __str(self):
+    def __str__(self):
         return f'{self.head} | Length: {self._length}'
 
     def __repr__(self):
@@ -26,39 +26,57 @@ class LinkedList(object):
     def insert(self, val):
         """ Inserting a Node created according to its value inserted to this method
         """
-        new_node = Node(val)
-        new_node._next = self.head
-        self.head = new_node
-        self.current = self.head
+        self.head = Node(val, self.head)
         self._length += 1
 
     def includes(self, val) -> bool:
         """ Validates if inputed value is included in the linked list
         """
-        self.current = self.head
-        while self.current is not None:
-            if self.current.val is val:
+        current = self.head
+        while current is not None:
+            if current.val is val:
                 return True
-            self.current = self.current._next
+            current = current._next
         return False
 
     def append(self, value):
         """ method that adds a new node at the end of the linked list
         """
-        self.current = self.head
-        while self.current._next is not None:
-            self.current = self.current._next
-        new_node = Node(value)
-        self.current._next = new_node
+        current = self.head
+        while current._next is not None:
+            current = current._next
+        current._next = Node(value)
+        self._length += 1
 
+    def insertBefore(self, val, newVal):
+        """ method that adds a new node before a given value of a node
+        """
+        current = self.head
+        while current._next.val is not val:
+            current = current._next
+        current._next = Node(newVal, current._next)
+        self._length += 1
 
-def run():
-    ll = LinkedList([1, 2, 3])
-    print(ll.includes(1))
-    print(ll.includes(2))
-    print(ll.includes(3))
+    def insertAfter(self, val, newVal):
+        """ method that adds a new node after a given value of a node
+        """
+        current = self.head
+        while current.val is not val:
+            current = current._next
+        current._next = Node(newVal, current._next)
+        self._length += 1
 
 
 if __name__ == '__main__':
-    run()
-
+    ll = LinkedList()
+    ll.insert(2)
+    ll.insert(3)
+    ll.insert(6)
+    ll.insert(7)
+    ll.append(1)
+    ll.insertBefore(3, 4)
+    ll.insertAfter(6, 5)
+    ll.current = ll.head
+    while ll.current is not None:
+        print(str(ll.current.val))
+        ll.current = ll.current._next
