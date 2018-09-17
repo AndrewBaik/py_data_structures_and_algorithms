5014cc48de19cdc269ef4a51a3be7c4526cad33a
Andrew Baik <andrewbaik@outlook.com>
2018-09-17 14:47:42 -0700

    graph breadthfirst

--- data_structures/graph/graph.py
+++ data_structures/graph/graph.py
@@ -1,3 +1,66 @@
+class Node(object):
+    """ node used
+    """
+    def __init__(self, value=None):
+        self.value = value
+        self.neighbors = []
+        self.visited = False
+
+
+class Queue:
+    """ Queue class
+    """
+    def __init__(self, val_list=None):
+        """ Constructor for Queue
+        """
+        self.front = None
+        self.rear = None
+        self._length = 0
+
+        if val_list is not None:
+            for val in val_list:
+                self.enqueue(val)
+
+    def __len__(self):
+        """ returns the length of the queue
+        """
+        return self._length
+
+    def __repr__(self):
+        """ returns the detail information about the queue
+        """
+        return f'< Queue | Front {self.front} | Rear {self.rear} | Length: {self._length} >'
+
+    def __str__(self):
+        """ returns the information about the queue
+        """
+        return f'{self.front} | {self.rear} | Length: {self._length} '
+
+    def enqueue(self, val):
+        """ Adds a new node into the queue
+        """
+        self.rear = Node(val, self.front)
+        if self.front is None:
+            self.front = self.rear
+        self._length += 1
+
+    def dequeue(self):
+        """ Removes the last node in the queue
+        """
+        if self.front is not self.rear:
+            current = self.rear
+            while current._next is not self.front:
+                current = current._next
+            temp = current._next
+            current._next = None
+            self.front = current
+        else:
+            temp = self.front
+            self.rear, self.front = None
+        self._length -= 1
+        return temp
+
+
 class Graph(object):
     def __init__(self):
         self.graph = {}
@@ -32,7 +95,6 @@ class Graph(object):
     def add_edge(self, value1, value2, weight):
         """ adds a new edge between two vertices
         """
-        # import pdb; pdb.set_trace()
         try:
             self.graph[value1]
             self.graph[value2]
@@ -56,4 +118,30 @@ class Graph(object):
         else:
             raise KeyError
 
+    def breadth_first(self, root):
+        """ returns a list of all the nodes in the graph
+        """
+        if root is None:
+            return([])
+        output = []
+        for key in self.graph.keys():
+            output.append(key)
+        return output
 
+    # def breadth_first_traversal(self, Node):
+    #    """
+    #    """
+    #    if Node is None:
+    #        return([])
+    #    queue = Queue()
+    #    output = []
+    #    queue.enqueue(Node)
+    #    Node.visited = True
+    #    while len(queue) > 0:
+    #        front = queue.dequeue()
+    #        output.append(queue.dequeue().value)
+    #        for i in front.neighbors:
+    #            if i.visited is False:
+    #                queue.enqueue(i)
+    #                i.visited = True
+    #    return output
