a6187558ca6e6859921d02fdc9d9c1a087daacb2
AndrewBaik <36938031+AndrewBaik@users.noreply.github.com>
2019-01-11 14:53:33 -0800

    Update array_shift.py
    Provided answer with improved Big O

--- challenges/array_shift/array_shift.py
+++ challenges/array_shift/array_shift.py
@@ -16,3 +16,20 @@ def insertShiftArray(input_list, input_element):
             new_counter += 1
             counter += 1
     return new_list
+
+
+
+# Following code will answer the problem with BIG O of 1/2 n
+# answer = []
+
+# for i in range(len(input_list)):
+#   if len(input_list) - i == i:
+#     answer.append(input_element)
+#     return answer + input_list[i:]
+  
+#   elif len(input_list) % 2 == 1 and len(input_list) - i - 1 == i:
+#     answer.append(input_element)
+#     return answer + input_list[i:]
+  
+#   answer.append(input_list[i])
+  
